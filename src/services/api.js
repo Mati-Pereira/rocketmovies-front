38f9a1a8b884ab51c66eb9bfcdccddcac1994b55
Matheus <matheus-rodrigues37@live.com>
2022-10-19 17:04:42 -0300

    sem env

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: "https://rocketmovies-back-production.up.railway.app/",
 });
