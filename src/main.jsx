de99b38d5dad193a4d905227180e3d15c02da10c
Matheus <matheus-rodrigues37@live.com>
2022-12-22 15:30:02 -0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
-
 import { AuthProvider } from "./hooks/auth";
-
 import theme from "./styles/theme";
-
 import { Routes } from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -16,6 +14,18 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <GlobalStyles />
       <AuthProvider>
         <Routes />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,79 +1,141 @@
-import { useState } from "react";
-import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi";
-import { Link, useNavigate } from "react-router-dom";
-
-import { api } from "../../services/api";
-
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
-
 import { Container, Form, Background } from "./styles";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
+import { useState } from "react";
+import { api } from "../../services/api";
+import { Controller, useForm } from "react-hook-form";
+import { LineWobble } from "@uiball/loaders";
+import { toast } from "react-toastify";
 
-export function SignUp() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
   const navigate = useNavigate();
 
-  function handleSignUp() {
-    if (!name || !email || !password) {
-      return alert("Preencha todos os campos!");
-    }
-
-    api
-      .post("/users", { name, email, password })
-      .then(() => {
-        alert("Usuário cadastrado com sucesso!");
-        navigate("/");
-      })
-      .catch((error) => {
-        if (error.response) {
-          alert(error.response.data.message);
-        } else {
-          alert("Não foi possível cadastrar");
-        }
+  const onSubmit = async ({ name, email, password }) => {
+    try {
+      setIsLoading(true);
+      await api.post("/users", { name, email, password });
+      toast.success("Cadastro realizado com sucesso!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
       });
-  }
+      navigate(-1);
+      setIsLoading(false);
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+        setIsLoading(false);
+      } else {
+        toast.error("Erro no cadastro!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+        setIsLoading(false);
+      }
+    }
+  };
 
   return (
     <Container>
-      <Form>
-        <h1>RocketMovies</h1>
-        <p>Aplicação para acompanhar tudo que assistir.</p>
-
-        <h2>Crie sua conta</h2>
-
-        <Input
-          placeholder="Nome"
-          type="text"
-          icon={FiUser}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <Input
-          placeholder="E-mail"
-          type="text"
-          icon={FiMail}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <Input
-          placeholder="Senha"
-          type="password"
-          icon={FiLock}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <Button title="Cadastrar" onClick={handleSignUp} />
-
-        <Link to="/">
-          <FiArrowLeft />
-          Voltar para o login
-        </Link>
-      </Form>
-
       <Background />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis</p>
+        <h2>Crie sua conta</h2>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Name"
+              type="text"
+              icon={FiUser}
+              onChange={onChange}
+              value={value}
+              required
+            />
+          )}
+          name="name"
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Email"
+              type="email"
+              icon={FiMail}
+              onChange={onChange}
+              value={value}
+              required
+            />
+          )}
+          name="email"
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Password"
+              type="password"
+              icon={FiLock}
+              onChange={onChange}
+              value={value}
+              required
+            />
+          )}
+          name="password"
+        />
+        <Button type="submit">
+          {isLoading ? (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <LineWobble size={30} lineWeight={5} speed={2} color="black" />
+            </div>
+          ) : (
+            "Cadastrar"
+          )}
+        </Button>
+        <Link to="/">Voltar para o Login</Link>
+      </Form>
     </Container>
   );
 }
+
+export { SignUp };
